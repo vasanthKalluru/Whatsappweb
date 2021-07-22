e4cd0617ecd7ae52530fb9fd010e605bb2523507
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 21:03:38 +0530

    material ui and create components in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Sidebar.css";
 
+
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar_header">
+
+            </div>
+            <div className="sidebar_search">
+
+            </div>
+            <div className="sidebar_chat">
+
+            </div>
         </div>
     )
 }
