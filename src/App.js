a3a766224deb5bc032df684da2654c7dbaf80bfd
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 19:15:59 +0530

    place items in center and shadow remove h1 in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-        <h2>Let's build a whatsapp</h2>
+
         <div className="app_body">
             {/*sidebar */}
             {/*chat*/}
