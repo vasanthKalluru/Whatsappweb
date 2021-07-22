35ee80abe01c611688ba50e45f43e2f9269dddf6
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 18:47:23 +0530

    firebase config files delete unwanted files start server

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC76l0q4xjGknl7VjmJSvVUfMgzjEWByqw",
+    authDomain: "whatsappcloneforinspire.firebaseapp.com",
+    projectId: "whatsappcloneforinspire",
+    storageBucket: "whatsappcloneforinspire.appspot.com",
+    messagingSenderId: "104891717895",
+    appId: "1:104891717895:web:41bcd54bfd461b69e30da2"
+  };
