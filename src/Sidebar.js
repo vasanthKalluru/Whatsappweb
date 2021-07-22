c6f21f2df3ab6d890ab40310eb6ddedc39a4be27
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 21:36:39 +0530

    create sidebar search and style it

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
+import { SearchOutlined } from '@material-ui/icons';
 
 function Sidebar() {
     return (
@@ -25,7 +26,8 @@ function Sidebar() {
             </div>
 
             <div className="sidebar_search">
-
+                <SearchOutlined/>
+                <input placeholder="search or start new chat"/>
             </div>
             <div className="sidebar_chat">
 
