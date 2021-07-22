2a97d825e7728eec8a4e7e239ff845346331e87f
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 21:13:04 +0530

    import icons and create header_right div in  sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Sidebar.css";
-
+import { Avatar } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
+                <Avatar/>
+                <div className="header_right">
+                    <ChatIcon/>
+                    <DonutLargeIcon/>
+                    <MoreVertIcon/>
+                </div>
 
             </div>
             <div className="sidebar_search">
