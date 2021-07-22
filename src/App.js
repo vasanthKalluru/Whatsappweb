d5830bb082e9e23523220305993621b91d797699
vasanth <vasanthkalluru3519@gmail.com>
2021-07-22 20:56:45 +0530

    declare sidebar and chat components and their css files, import them to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-
+import React from "react";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 function App() {
   return (
     <div className="app">
 
         <div className="app_body">
-            {/*sidebar */}
-            {/*chat*/}
+            <Sidebar/>
+            <Chat/>
         </div>
     </div>
   );
