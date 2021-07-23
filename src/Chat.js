5d6fccdf3f1456bdfcecaae2b7b014b5cc2bf55b
vasanth <vasanthkalluru3519@gmail.com>
2021-07-23 15:12:57 +0530

    make chat for receiver

--- src/Chat.js
+++ src/Chat.js
@@ -30,7 +30,7 @@ function Chat() {
                 </div>
             </div>
             <div className="chat_body">
-                <p className="chat_message">
+                <p className="chat_message chat_receiver_message">
                     <span className="chat_name">Abhinav </span>
                     Hi there
                     <span className="chat_timestamp">3:04 pm</span>
