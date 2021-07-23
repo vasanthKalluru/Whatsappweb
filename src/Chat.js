2627aff838908b103b8c6aaba63226367d27076e
vasanth <vasanthkalluru3519@gmail.com>
2021-07-23 13:58:47 +0530

    creating all chat components

--- src/Chat.js
+++ src/Chat.js
@@ -4,7 +4,15 @@ import "./Chat.css";
 function Chat() {
     return (
         <div className="chat">
-            
+            <div className="chat_header">
+
+            </div>
+            <div className="chat_body">
+
+            </div>
+            <div className="chat_footer">
+                
+            </div>
         </div>
     )
 }
