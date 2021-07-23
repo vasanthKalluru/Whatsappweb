33d50ad26ed57c22e37b7a9febf6a68a7f98d667
vasanth <vasanthkalluru3519@gmail.com>
2021-07-23 14:45:58 +0530

    creating chat header and styling chat body

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "./Chat.css";
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+
 
 function Chat() {
+    const [seed,setseed]=useState("");
+    useEffect(() => {
+        setseed(Math.floor(Math.random*1000));
+    }, [])
     return (
         <div className="chat">
             <div className="chat_header">
-
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="chat_header_info">
+                    <h3>Room name</h3>
+                    <p>Last seen at .... </p>
+                </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
             </div>
             <div className="chat_body">
 
             </div>
             <div className="chat_footer">
-                
+
             </div>
         </div>
     )
