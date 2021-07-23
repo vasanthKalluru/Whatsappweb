6ed9ad5670c95df2d58b53dd3f868a16abcd80f8
vasanth <vasanthkalluru3519@gmail.com>
2021-07-23 16:14:45 +0530

    firebase install and create variables in firebase.js create db in firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+ 
 const firebaseConfig = {
     apiKey: "AIzaSyC76l0q4xjGknl7VjmJSvVUfMgzjEWByqw",
     authDomain: "whatsappcloneforinspire.firebaseapp.com",
@@ -6,4 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "104891717895",
     appId: "1:104891717895:web:41bcd54bfd461b69e30da2"
   };
-  export default firebaseConfig;
+  const firebaseApp =firebase.initializeApp(firebaseConfig);
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+  const provider= new firebase.auth.GoogleAuthProvider();
+  
+  export {auth,provider};
+  export default db;
