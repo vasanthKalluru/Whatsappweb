62ca396c7c2303742cea2eda21ba6b25c16f99c1
vasanth <vasanthkalluru3519@gmail.com>
2021-07-23 13:02:34 +0530

    create Sidebar.js and sidebar.css

---
+++ src/SidebarChat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SidebarChat.css";
+function SidebarChat() {
+    return (
+        <div>
+            <h1>Hey</h1>
+        </div>
+    )
+}
+export default SidebarChat;
