5e92bb918911b878cb5d905d1125aa43013f376d
vasanth <vasanthkalluru3519@gmail.com>
2021-07-28 20:02:09 +0530

    create Login page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import {useState} from 'react'
 function App() {
+  const [user,setUser]=useState(null);
+
   return (
     <div className="app">
-        <div className="app_body">
-          <Router>
-            <Sidebar/>
-            <Switch>
-                <Route path="/rooms/:roomId" component={Chat}/>
-                <Route path="/" component={Chat}/>
-            </Switch>
-          </Router>
-        </div>
+      
+    {!user?(
+    <h1>Login</h1>
+    ):(
+      <div className="app_body">
+        <Router>
+          <Sidebar/>
+          <Switch>
+              <Route path="/rooms/:roomId" component={Chat}/>
+              <Route path="/" component={Chat}/>
+          </Switch>
+        </Router>
+   
     </div>
-  );
+  )
+  }
+  </div>
+    );
 }
 
 export default App;
