acf1ebf6a08b63fa55dd2d550fc54a0d964f12a5
vasanth <vasanthkalluru3519@gmail.com>
2021-07-29 07:38:38 +0530

    login with authentication and change firebase authentication

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {Button} from '@material-ui/core'
 import "./Login.css"
+import { auth, provider } from './firebase'
 function Login() {
-    const signIn=()=>{};
+    const signIn=()=>{
+        auth.signInWithPopup(provider).then(result=>console.log(result)).catch(error=>alert(error));
+    }
     return (
         <div className="login">
             <div className="login-container">
